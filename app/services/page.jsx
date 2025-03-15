daaabcecba0e77fde199c2f0fd3933271f78d8ef
Mujtaba <mujtabakhalid20@gmail.com>
2025-03-15 20:34:32 +0500

    services page init

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const page = () => {
-  return <main className={`scroll-smooth`}></main>;
+  return <main className={`scroll-smooth`}>
+    
+  </main>;
 };
 
 export default page;
