d233a3e269e2b04e644bfc416a48751e37dcde98
Mujtaba <mujtabakhalid20@gmail.com>
2025-03-18 02:09:41 +0500

    potfolio page layout

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -1,5 +1,31 @@
+import { projects } from "@/data/projectsData";
+import Image from "next/image";
+import Link from "next/link";
+
 const page = () => {
-  return <div>PORTFOLIO</div>;
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6 p-4 max-w-3xl mx-auto">
+      {projects.map((project) => (
+        <Link key={project.id} href={`projects/${project.id}`}>
+          <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+            <Image
+              src={project.images[0]}
+              alt={project.title}
+              className="w-full h-48 object-cover hover:opacity- transition-all duration-300"
+              width={300}
+              height={300}
+            />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold">{project.titleShort}</h3>
+              <p className="text-sm text-gray-600">
+                {project.projectSalients.location}
+              </p>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default page;
--- data/projectsData.js
+++ data/projectsData.js
@@ -2,6 +2,7 @@ export const projects = [
   {
     id: "dix-avenue-rehabilitation",
     title: "Dix Avenue Rehabilitation Project",
+    titleShort: "Dix Avenue",
     projectSalients: {
       location: "Queens, New York",
       client: "WFHA Rockaway Holdings LP",
@@ -40,9 +41,150 @@ export const projects = [
       },
     ],
     images: [
-      "/images/dix_avenue_1.jpg",
-      "/images/dix_avenue_2.jpg",
-      "/images/dix_avenue_3.jpg",
+      "/images/dix_avenue_1.webp",
+      "/images/dix_avenue_2.webp",
+      "/images/dix_avenue_3.webp",
+    ],
+  },
+  {
+    id: "dix-avenue-rehabilitation1",
+    title: "Dix Avenue Rehabilitation Project",
+    titleShort: "Dix Avenue",
+    projectSalients: {
+      location: "Queens, New York",
+      client: "WFHA Rockaway Holdings LP",
+      architect: "Oaklander, Coogan & Vitto Architects, P.C",
+      contractor: "Shamas Contracting Co. Inc.",
+      contractValue: "$15,578,370",
+      duration: "18 Months",
+    },
+    projectDescription:
+      "The Dix Avenue Rehabilitation project is a comprehensive renovation initiative aimed at revitalizing a multi-building residential complex along Dix Avenue. The project focuses on modernizing the buildings while preserving structural integrity and adhering to all safety, energy, and sustainability standards.",
+    scopeOfWork: [
+      {
+        category: "Structural Renovations",
+        description:
+          "Replacement of defective concrete slabs, curbs, and staircases to ensure long-term durability.",
+      },
+      {
+        category: "Masonry Repairs",
+        description:
+          "Restoration and maintenance of masonry surfaces to preserve the original architectural character.",
+      },
+      {
+        category: "Openings & Finishes",
+        description:
+          "Installation of new doors, windows, and interior finishes for improved aesthetics and functionality.",
+      },
+      {
+        category: "Mechanical & Electrical Systems",
+        description:
+          "Upgrades to plumbing, HVAC, and electrical systems to enhance energy efficiency and occupant comfort.",
+      },
+      {
+        category: "Site Improvements",
+        description:
+          "Exterior improvements, including asphalt paving, landscaping, and parking lot enhancements to elevate the site’s visual appeal and functionality.",
+      },
+    ],
+    images: [
+      "/images/dix_avenue_1.webp",
+      "/images/dix_avenue_2.webp",
+      "/images/dix_avenue_3.webp",
+    ],
+  },
+  {
+    id: "dix-avenue-rehabilitation2",
+    title: "Dix Avenue Rehabilitation Project",
+    titleShort: "Dix Avenue",
+    projectSalients: {
+      location: "Queens, New York",
+      client: "WFHA Rockaway Holdings LP",
+      architect: "Oaklander, Coogan & Vitto Architects, P.C",
+      contractor: "Shamas Contracting Co. Inc.",
+      contractValue: "$15,578,370",
+      duration: "18 Months",
+    },
+    projectDescription:
+      "The Dix Avenue Rehabilitation project is a comprehensive renovation initiative aimed at revitalizing a multi-building residential complex along Dix Avenue. The project focuses on modernizing the buildings while preserving structural integrity and adhering to all safety, energy, and sustainability standards.",
+    scopeOfWork: [
+      {
+        category: "Structural Renovations",
+        description:
+          "Replacement of defective concrete slabs, curbs, and staircases to ensure long-term durability.",
+      },
+      {
+        category: "Masonry Repairs",
+        description:
+          "Restoration and maintenance of masonry surfaces to preserve the original architectural character.",
+      },
+      {
+        category: "Openings & Finishes",
+        description:
+          "Installation of new doors, windows, and interior finishes for improved aesthetics and functionality.",
+      },
+      {
+        category: "Mechanical & Electrical Systems",
+        description:
+          "Upgrades to plumbing, HVAC, and electrical systems to enhance energy efficiency and occupant comfort.",
+      },
+      {
+        category: "Site Improvements",
+        description:
+          "Exterior improvements, including asphalt paving, landscaping, and parking lot enhancements to elevate the site’s visual appeal and functionality.",
+      },
+    ],
+    images: [
+      "/images/dix_avenue_1.webp",
+      "/images/dix_avenue_2.webp",
+      "/images/dix_avenue_3.webp",
+    ],
+  },
+  {
+    id: "dix-avenue-rehabilitation3",
+    title: "Dix Avenue Rehabilitation Project",
+    titleShort: "Dix Avenue",
+    projectSalients: {
+      location: "Queens, New York",
+      client: "WFHA Rockaway Holdings LP",
+      architect: "Oaklander, Coogan & Vitto Architects, P.C",
+      contractor: "Shamas Contracting Co. Inc.",
+      contractValue: "$15,578,370",
+      duration: "18 Months",
+    },
+    projectDescription:
+      "The Dix Avenue Rehabilitation project is a comprehensive renovation initiative aimed at revitalizing a multi-building residential complex along Dix Avenue. The project focuses on modernizing the buildings while preserving structural integrity and adhering to all safety, energy, and sustainability standards.",
+    scopeOfWork: [
+      {
+        category: "Structural Renovations",
+        description:
+          "Replacement of defective concrete slabs, curbs, and staircases to ensure long-term durability.",
+      },
+      {
+        category: "Masonry Repairs",
+        description:
+          "Restoration and maintenance of masonry surfaces to preserve the original architectural character.",
+      },
+      {
+        category: "Openings & Finishes",
+        description:
+          "Installation of new doors, windows, and interior finishes for improved aesthetics and functionality.",
+      },
+      {
+        category: "Mechanical & Electrical Systems",
+        description:
+          "Upgrades to plumbing, HVAC, and electrical systems to enhance energy efficiency and occupant comfort.",
+      },
+      {
+        category: "Site Improvements",
+        description:
+          "Exterior improvements, including asphalt paving, landscaping, and parking lot enhancements to elevate the site’s visual appeal and functionality.",
+      },
+    ],
+    images: [
+      "/images/dix_avenue_1.webp",
+      "/images/dix_avenue_2.webp",
+      "/images/dix_avenue_3.webp",
     ],
   },
 ];
